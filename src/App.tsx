2d1bb88dea3afbebee9459610d4f0c4183f6d75a
Eric <ericjo2478@gmail.com>
2025-08-31 19:51:17 -0600

    rewrote the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import {
   collection,
+  DocumentData,
   getFirestore,
   onSnapshot,
-  updateDoc,
+  QuerySnapshot,
 } from "firebase/firestore";
 import { getAuth, onAuthStateChanged, User } from "firebase/auth";
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { useEffect, useState } from "react";
+import { Key, useEffect, useState } from "react";
 import MedCard from "./components/MedCard";
-import { Accordion, Col, Container, Row } from "react-bootstrap";
+import { Accordion, Col, Container, Nav, Row } from "react-bootstrap";
 import Med, { fetchMeds } from "./components/Med";
 import NavBar from "./components/NavBar";
 import LoginForm from "./components/LoginForm";
@@ -18,11 +17,9 @@ import Order, { fetchOrders } from "./components/Order";
 import MedSettings from "./components/MedSettings";
 import OrderAccordionItem from "./components/OrderAccordionItem";
 import OrderForm from "./components/OrderForm";
+import OrderEntry from "./components/OrderEntry";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD9-6prflaJhhuFut_6-ze9H5JI2ROG0b8",
   authDomain: "bllkinventory-bd8eb.firebaseapp.com",
@@ -36,16 +33,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
 export const auth = getAuth(app);
-const ordersRef = collection(database, "orders");
 
-export const today = new Date();
-export const expiryDay = new Date();
-expiryDay.setDate(today.getDate() + 14);
-export const zeroedDay = new Date();
-zeroedDay.setDate(today.getDate() - 5);
-zeroedDay.setHours(0, 0, 0, 0);
+// export const today = new Date();
+// export const expiryDay = new Date();
+// expiryDay.setDate(today.getDate() + 14);
+// export const zeroedDay = new Date();
+// zeroedDay.setDate(today.getDate() - 5);
+// zeroedDay.setHours(0, 0, 0, 0);
 
-export async function firestoreWithNetworkRetry(
+// execute an async function with handling to retry up to 3 times at 1 sec intervals when getting a netword error
+export async function functionNetworkRetry(
   operationFunction: () => Promise<any>,
   maxRetries = 3,
   delayMs = 1000
@@ -57,7 +54,7 @@ export async function firestoreWithNetworkRetry(
       return result; // Success
     } catch (error: any) {
       if (error.code === "unavailable" || error.code === "deadline-exceeded") {
-        // Example error codes for network/timeout
+        // Network error handling
         console.warn(
           `Firestore operation failed (retry ${retries + 1}/${maxRetries}):`,
           error.message
@@ -71,184 +68,264 @@ export async function firestoreWithNetworkRetry(
       }
     }
   }
-  throw new Error(`Firestore operation failed after ${maxRetries} retries.`);
+  throw new Error(`Firestore operation failed after ${maxRetries} retries.`); // too many tries
 }
 
+// object of uuid (string) and any
 export interface KeyList<T> {
   [key: string]: T;
 }
 
 export default function App() {
-  const [meds, setMeds] = useState({} as KeyList<Med>);
-  const [orders, setOrders] = useState({} as KeyList<Order>);
-  const [user, setCurrentUser] = useState(null as User | null);
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState("home");
-  const [pendingOrder, setPendingOrder] = useState(null as Order | null);
+  const [meds, setMeds] = useState({} as KeyList<Med>); // list of loaded meds
+  const [orders, setOrders] = useState({} as KeyList<Order>); // list of loaded orders
+  const [user, setCurrentUser] = useState(null as User | null); // authentiaced user
+  const [loading, setLoading] = useState(true); // state for while page is loading
+  const [page, setPage] = useState("home"); // current page being displayed
+  const [pendingOrder, setPendingOrder] = useState(null as Order | null); // the pending order (if any)
 
+  // should be run whenever a property of a med changes to refresh react
+  const handleMedsChange = () => {
+    setMeds({ ...meds });
+  };
+  // should be run whenever a property of an order changes to refresh react
+  const handleOrdersChange = () => {
+    setOrders({ ...orders });
+  };
+
+  // get the authenticated user if already logged in
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
-      setLoading(false);
+      setCurrentUser(user); // save user
+      setLoading(false); // mark page as loaded
     });
 
     // Clean up the listener when the component unmounts
     return () => unsubscribe();
-  }, []); // Empty dependency array ensures this runs once on mount
+  }, []);
 
+  const getNewOrders = (
+    meds: KeyList<Med>,
+    snapshot?: QuerySnapshot<DocumentData, DocumentData>
+  ) => {
+    // fetch orders passing in the snapshot docs
+    fetchOrders(handleOrdersChange, meds, snapshot?.docs).then((data) => {
+      // set the orders and pending from data
+      const [orderData, pending] = data;
+      // save the orders and pending order
+      setOrders(orderData as KeyList<Order>);
+      setPendingOrder(pending as Order);
+      handleMedsChange(); // meds onOrder value changed
+    });
+  };
+
+  // fetch the meds and orders once the user is loaded
   useEffect(() => {
     if (!loading && user) {
-      fetchMeds().then((d) => {
-        setMeds(d);
-        fetchOrders(d).then((d) => setOrders(d));
+      fetchMeds(handleMedsChange).then((meds) => {
+        setMeds(meds);
+        getNewOrders(meds);
       });
     }
   }, [user]);
 
+  // setup a snapshot to track order changes in the database if the meds and user are loaded
   useEffect(() => {
     if (!loading && Object.keys(meds).length > 0) {
-      const unsubscribe = onSnapshot(ordersRef, (snapshot) => {
-        const dataSet: KeyList<Order> = {};
-        Object.values(meds).forEach((med) => (med.onOrder = 0));
-        let pending: Order | null = null;
-        for (const doc of snapshot.docs) {
-          const docData = doc.data();
-          const medEntries = [];
-          for (const entry of docData.meds) {
-            medEntries.push({ med: meds[entry.id], amount: entry.amount });
-          }
-
-          dataSet[doc.id] = new Order(
-            doc.id,
-            docData.date.toDate(),
-            medEntries,
-            docData.status
-          );
-          if (docData.status === "Pending") {
-            pending = dataSet[doc.id];
-          }
+      const unsubscribe = onSnapshot(
+        collection(database, "orders"),
+        (snapshot) => {
+          getNewOrders(meds, snapshot);
         }
-        const entries = Object.entries(dataSet);
-        entries.sort((a, b) => a[1].compare(b[1]));
-        const sortedData = Object.fromEntries(entries);
-        setOrders(sortedData);
-        setPendingOrder(pending);
-      });
+      );
 
       // Cleanup function to unsubscribe when the component unmounts
       return () => unsubscribe();
     }
-  }, [meds]); // Empty dependency array means this effect runs once on mount
+  }, []);
 
+  // if page is loading display a message
   if (loading) {
-    return <div>Loading authentication status...</div>;
-  }
-
-  if (user) {
     return (
       <>
-        <NavBar
-          admin={user.email === "admin@bllk.inv"}
-          setCurrentUser={setCurrentUser}
-          setPage={setPage}
-        ></NavBar>
-        {page === "home" && (
-          <Container>
-            <Row>
-              {Object.values(meds).map(
-                (med) =>
-                  med.display && (
-                    <Col key={med.getId()} className="mb-3">
-                      <MedCard pending={pendingOrder}>{med}</MedCard>
-                    </Col>
-                  )
-              )}
-            </Row>
-          </Container>
-        )}
-        {page === "orders" && (
-          <Accordion>
-            {pendingOrder === null && (
-              <OrderAccordionItem
-                eventKey="pending"
-                meds={meds}
-                order={null}
-                onReceive={() => null}
-              ></OrderAccordionItem>
-            )}
-            {Object.values(orders).map((order, index) => (
-              <OrderAccordionItem
-                key={order.getId()}
-                eventKey={index.toString()}
-                meds={meds}
-                order={order}
-                onReceive={() => {
-                  if (order.getStatus() === "Ordered") {
-                    for (const entry of order.getEntries()) {
-                      entry.med.onOrder = entry.med.onOrder - entry.amount;
-                    }
-                  }
-                  order.setStatus("Received");
-                  setOrders({ ...orders });
-
-                  updateDoc(order.getRef(), { status: "Received" });
-                }}
-              ></OrderAccordionItem>
-            ))}
-          </Accordion>
-        )}
-        <OrderForm
-          show={page === "submit"}
-          pendingOrder={pendingOrder}
-          meds={meds}
-          onSubmit={() => setPage("orders")}
-        ></OrderForm>
-
-        {page === "meds" && (
-          <Container>
-            <Row>
-              {Object.values(meds).map(
-                (med) =>
-                  med.display && (
-                    <Col key={med.getId()} className="mb-3">
-                      <MedSettings
-                        handleMedChange={() => {
-                          fetchMeds().then((d) => {
-                            setMeds(d);
-                          });
-                        }}
-                      >
-                        {med}
-                      </MedSettings>
-                    </Col>
-                  )
-              )}
-              <Col className="mb-3">
-                <MedSettings
-                  handleMedChange={() => {
-                    fetchMeds().then((d) => {
-                      setMeds(d);
-                    });
-                  }}
-                >
-                  {undefined}
-                </MedSettings>
-              </Col>
-            </Row>
-          </Container>
-        )}
-      </>
-    );
-  } else {
-    return (
-      <>
-        <LoginForm setCurrentUser={setCurrentUser}></LoginForm>
         <NavBar
           admin={false}
           setCurrentUser={setCurrentUser}
           setPage={setPage}
         ></NavBar>
+        <div>Loading authentication status...</div>
       </>
     );
   }
+
+  return (
+    <>
+      {/* display nav bar */}
+      <NavBar
+        admin={user && user.email === "admin@bllk.inv" ? true : false} // check if user is admin
+        setCurrentUser={setCurrentUser}
+        setPage={setPage}
+      ></NavBar>
+      {
+        // display home page by rendering each med
+        page === "home" && (
+          <Container>
+            <Row>
+              {Object.values(meds).map((med) => med.render(pendingOrder))}
+            </Row>
+          </Container>
+        )
+      }
+      {
+        // display orders page by rendering each order
+        page === "orders" && (
+          <Accordion>
+            {
+              // if there is no pending order display an artificial pending order
+              pendingOrder === null && Order.renderPendingOrder(meds)
+            }
+            {
+              // render the orders
+              Object.values(orders).map((order) => order.render())
+            }
+          </Accordion>
+        )
+      }
+      {/* render orderform in the background but hide when on another page so the form isn't reset on page change */}
+      <OrderForm
+        show={page === "submit"}
+        pendingOrder={pendingOrder}
+        meds={meds}
+        onSubmit={() => setPage("orders")}
+      />
+      {
+        // display meds settings page by rendering each med settings
+        page === "meds" && (
+          <Row>
+            {Object.values(meds).map((med) =>
+              med.renderSettings(setMeds, handleMedsChange)
+            )}
+            {/* Meds Settings submition form */}
+            <Col className="mb-3">
+              <MedSettings
+                handleMedChange={() => {
+                  fetchMeds(handleMedsChange).then((d) => {
+                    setMeds(d);
+                  });
+                }}
+              />
+            </Col>
+          </Row>
+        )
+      }
+    </>
+  );
+  // if done loading and have user
+  //   if (user) {
+  //     return (
+  //       <>
+  //         <NavBar
+  //           admin={user.email === "admin@bllk.inv"}
+  //           setCurrentUser={setCurrentUser}
+  //           setPage={setPage}
+  //         ></NavBar>
+  //         {page === "home" && (
+  //           <Container>
+  //             <Row>
+  //               {Object.values(meds).map(
+  //                 (med) =>
+  //                   med.display && (
+  //                     <Col key={med.getId()} className="mb-3">
+  //                       <MedCard pending={pendingOrder}>{med}</MedCard>
+  //                     </Col>
+  //                   )
+  //               )}
+  //             </Row>
+  //           </Container>
+  //         )}
+  //         {page === "orders" && (
+  //           <Accordion>
+  //             {pendingOrder === null && (
+  //               <OrderAccordionItem
+  //                 eventKey="pending"
+  //                 meds={meds}
+  //                 order={null}
+  //                 onReceive={() => null}
+  //               ></OrderAccordionItem>
+  //             )}
+  //             {Object.values(orders).map((order, index) => (
+  //               <OrderAccordionItem
+  //                 key={order.getId()}
+  //                 eventKey={index.toString()}
+  //                 meds={meds}
+  //                 order={order}
+  //                 onReceive={() => {
+  //                   if (order.getStatus() === "Ordered") {
+  //                     for (const entry of order.getEntries()) {
+  //                       entry.med.onOrder = entry.med.onOrder - entry.amount;
+  //                     }
+  //                   }
+  //                   order.setStatus("Received");
+  //                   setOrders({ ...orders });
+
+  //                   updateDoc(order.getRef(), { status: "Received" });
+  //                 }}
+  //               ></OrderAccordionItem>
+  //             ))}
+  //           </Accordion>
+  //         )}
+  //         <OrderForm
+  //           show={page === "submit"}
+  //           pendingOrder={pendingOrder}
+  //           meds={meds}
+  //           onSubmit={() => setPage("orders")}
+  //         ></OrderForm>
+
+  //         {page === "meds" && (
+  //           <Container>
+  //             <Row>
+  //               {Object.values(meds).map(
+  //                 (med) =>
+  //                   med.display && (
+  //                     <Col key={med.getId()} className="mb-3">
+  //                       <MedSettings
+  //                         handleMedChange={() => {
+  //                           fetchMeds().then((d) => {
+  //                             setMeds(d);
+  //                           });
+  //                         }}
+  //                       >
+  //                         {med}
+  //                       </MedSettings>
+  //                     </Col>
+  //                   )
+  //               )}
+  //               <Col className="mb-3">
+  //                 <MedSettings
+  //                   handleMedChange={() => {
+  //                     fetchMeds().then((d) => {
+  //                       setMeds(d);
+  //                     });
+  //                   }}
+  //                 >
+  //                   {undefined}
+  //                 </MedSettings>
+  //               </Col>
+  //             </Row>
+  //           </Container>
+  //         )}
+  //       </>
+  //     );
+  //   } else {
+  //     return (
+  //       <>
+  //         <LoginForm setCurrentUser={setCurrentUser}></LoginForm>
+  //         <NavBar
+  //           admin={false}
+  //           setCurrentUser={setCurrentUser}
+  //           setPage={setPage}
+  //         ></NavBar>
+  //       </>
+  //     );
+  //   }
 }
