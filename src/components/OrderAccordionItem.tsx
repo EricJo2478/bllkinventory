0e228d014ec94ec08c9deb04b4c8c4100c5a366d
Eric <ericjo2478@gmail.com>
2025-09-04 11:36:27 -0600

    enchanced order accordion to work with order tools

--- src/components/OrderAccordionItem.tsx
+++ src/components/OrderAccordionItem.tsx
@@ -12,99 +12,102 @@ import { KeyList } from "../App";
 import Med from "./Med";
 
 interface Props {
-  order: Order | null;
+  order?: Order;
   eventKey: string;
-  onReceive: () => void;
-  meds: KeyList<Med>;
+  onReceive?: () => void;
+  meds?: KeyList<Med>;
 }
 
+// order display component
 export default function OrderAccordionItem({
   order,
   eventKey,
   onReceive,
   meds,
 }: Props) {
+  // array for the pending orders content
   const pendingContent: ReactNode[] = [];
-  if (order === null || order.getStatus() === "Pending") {
-    const medsList = Object.values(meds);
-    medsList.forEach((med) => {
-      med.calcOrder();
-    });
-    const medsToOrder = medsList.filter((med) => med.getToOrder() > 0);
+  if (order === undefined || order.getStatus() === "Pending") {
+    // list of all meds
+    const medsList = meds ? Object.values(meds) : [];
+
+    // iterate through meds and update the amount to order
+    medsList.forEach((med) => med.calculateOrder());
+
+    // filter the meds list ot the meds that are actually getting ordered
+    const medsToOrder = medsList.filter((med) => med.getAmountToOrder() > 0);
+
+    // if there is a pre-exisiting order
     if (order) {
-      order.entries.forEach((entry) => {
-        if (medsToOrder.includes(entry.med)) {
-          if (entry.amount > entry.med.getToOrder()) {
-            pendingContent.push(entry.med.getName() + ": x" + entry.amount);
-            medsToOrder.splice(medsToOrder.indexOf(entry.med), 1);
+      // add each orders meds to the content if there isn't more being auto-ordered
+      order.getEntries().forEach((entry) => {
+        const med = entry.getMed(); //
+        // check if med is both manually and auto-ordered
+        if (medsToOrder.includes(med)) {
+          // if ther is more manually ordered of a med than automatically
+          if (entry.getAmount() > med.getAmountToOrder()) {
+            // add the entry to the pending content and remove the med from the auot-order array
+            pendingContent.push(entry.toString());
+            medsToOrder.splice(medsToOrder.indexOf(med), 1);
           }
         } else {
-          pendingContent.push(entry.med.getName() + ": x" + entry.amount);
+          // if ther is no overlap then simply add the entry to the contents
+          pendingContent.push(entry.toString());
         }
       });
     }
+
+    // add each automatically ordered med to the pening contents
     medsToOrder.forEach((med) => {
-      pendingContent.push(med.getName() + ": x" + med.getToOrder());
+      pendingContent.push(med.getName() + ": x" + med.getAmountToOrder());
     });
+
+    // sort pending content
+    pendingContent.sort();
+
+    // if there is no pending content don't render and accordion item
+    if (pendingContent.length === 0) {
+      return null;
+    }
   }
 
-  if (order) {
-    return (
-      <Card>
-        <OrderHeader
-          disableButton={
-            order.getStatus() === "Received" || order.getStatus() === "Pending"
-          }
-          onButtonClick={onReceive}
-          eventKey={eventKey}
-        >
-          {order}
-        </OrderHeader>
-        <Accordion.Collapse eventKey={eventKey}>
-          <Card.Body>
-            {order.getStatus() === "Pending"
-              ? pendingContent.map((str, index) => {
-                  return (
-                    <p key={index} className="mb-0">
-                      {str}
-                    </p>
-                  );
-                })
-              : order.getContent().map((str, index) => {
-                  return (
-                    <p key={index} className="mb-0">
-                      {str}
-                    </p>
-                  );
-                })}
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    );
-  } else {
-    return (
-      <Card>
-        <OrderHeader
-          disableButton
-          onButtonClick={onReceive}
-          eventKey={eventKey}
-        >
-          {null}
-        </OrderHeader>
-        <Accordion.Collapse eventKey={eventKey}>
-          <Card.Body>
-            {pendingContent.map((str, index) => {
-              return (
-                <p key={index} className="mb-0">
-                  {str}
-                </p>
-              );
-            })}
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    );
-  }
+  return (
+    <Card>
+      <OrderHeader
+        disableButton={
+          // disable button if the order is received or pending
+          order
+            ? order.getStatus() === "Received" ||
+              order.getStatus() === "Pending"
+            : true
+        }
+        onButtonClick={onReceive ? onReceive : () => {}}
+        eventKey={eventKey}
+        order={order}
+      />
+      <Accordion.Collapse eventKey={eventKey}>
+        <Card.Body>
+          {order === undefined || order.getStatus() === "Pending"
+            ? // if order is pending then render the pending content as p elements
+              pendingContent.map((str, index) => {
+                return (
+                  <p key={index} className="mb-0">
+                    {str}
+                  </p>
+                );
+              })
+            : // iterate through the order content and render as p elements
+              order.getContent().map((str, index) => {
+                return (
+                  <p key={index} className="mb-0">
+                    {str}
+                  </p>
+                );
+              })}
+        </Card.Body>
+      </Accordion.Collapse>
+    </Card>
+  );
 }
 
 interface ModalProps {
@@ -112,6 +115,7 @@ interface ModalProps {
   onReceive: Function;
 }
 
+// modal to confirm an order should be marked received
 function RecieveModal({ handleClose, onReceive }: ModalProps) {
   return (
     <Modal show onHide={handleClose}>
@@ -121,6 +125,7 @@ function RecieveModal({ handleClose, onReceive }: ModalProps) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Footer>
+        {/* button to mark order received and close modal */}
         <Button
           variant="primary"
           onClick={() => {
@@ -129,6 +134,8 @@ function RecieveModal({ handleClose, onReceive }: ModalProps) {
         >
           Receive
         </Button>
+
+        {/* button to close modal */}
         <Button variant="secondary" onClick={handleClose}>
           Cancel
         </Button>
@@ -138,38 +145,39 @@ function RecieveModal({ handleClose, onReceive }: ModalProps) {
 }
 
 interface HeaderProps {
-  children: Order | null;
+  order?: Order;
   eventKey: string;
   onButtonClick: (e: any) => void;
   disableButton: boolean;
 }
 
+// header of an order accordion, including the receive button and status badge
 function OrderHeader({
-  children,
+  order,
   eventKey,
   onButtonClick,
   disableButton,
 }: HeaderProps) {
+  // state to track receive confirmation modal visibility
   const [show, setShow] = useState(false);
 
-  const decoratedOnClick = useAccordionButton(eventKey);
-  let date: string;
-  if (children) {
-    date = children.getDateString();
-  } else {
-    const monday = new Date();
-    monday.setDate(monday.getDate() + ((1 + 7 - monday.getDay()) % 7));
-    date = monday.toDateString();
-  }
+  // use accordion event for accordion functionality
+  const onClick = useAccordionButton(eventKey);
+
+  // get date of order (monday if pending order)
+  let date = order ? order.getDateString() : Order.pendingDate.toDateString();
 
   return (
     <>
-      {show && (
-        <RecieveModal
-          handleClose={() => setShow(false)}
-          onReceive={onButtonClick}
-        ></RecieveModal>
-      )}
+      {
+        // show receive modal if modal visiblity is true
+        show && (
+          <RecieveModal
+            handleClose={() => setShow(false)}
+            onReceive={onButtonClick}
+          ></RecieveModal>
+        )
+      }
       <Card.Header>
         <div
           style={{
@@ -178,16 +186,15 @@ function OrderHeader({
             alignItems: "center",
           }}
         >
-          <div
-            onClick={decoratedOnClick}
-            style={{ flexGrow: 1, cursor: "pointer" }}
-          >
+          <div onClick={onClick} style={{ flexGrow: 1, cursor: "pointer" }}>
+            {/*  display date as the header content */}
             {date}
+            {/* badge displaying order status using the corisponding colour */}
             <Badge
-              bg={children ? children.getStatusColour() : "info"}
+              bg={order ? order.getStatusColour() : "info"}
               className="ms-3"
             >
-              {children ? children.getStatus() : "Pending"}
+              {order ? order.getStatus() : "Pending"}
             </Badge>
           </div>
           <Button
